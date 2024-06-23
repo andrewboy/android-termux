fb4b70ef59de6ffe03be7feb5b2d2498ac132bfe
András József Beck <andras.jozsef.beck.dev@gmail.com>
2024-06-23 23:01:01 +0200

    add youtube download

--- custom.fish
+++ custom.fish
@@ -3,9 +3,35 @@ if test -f /data/data/com.termux/files/usr/bin/fastfetch
   fastfetch
 end
 
+function youtube_download_video
+  set -l url $argv[1]
+  set -l download_path "$HOME/storage/downloads/youtube-dl"
+  set -l file_name "%(title)s.%(ext)s"
+
+  yt-dlp -c -i "$url" -o "$download_path/$file_name"
+end
+
+function youtube_download_audio
+  set -l url $argv[1]
+  set -l download_path "$HOME/storage/downloads/youtube-dl"
+  set -l file_name "%(title)s.%(ext)s"
+
+  yt-dlp -x --audio-format "mp3" -c -i "$url" -o "$download_path/$file_name"
+end
+
+function download_youtube_video
+  set -l url=$argv[1]
+  # set -l download_path="$HOME/storage/downloads/youtube-dl"
+  set -l file_name="%(title)s.%(ext)s"
+
+  # yt-dlp -c -i "$url" -o "$download_path/$file_name"
+  yt-dlp -c -i "$url" -o "$file_name"
+
 starship init fish | source
 
 alias cat="bat"
 alias lsx="eza --icons --octal-permissions"
 alias ll="lsx -la"
 alias pn="pnpm"
+alias ytdlv="youtube_download_video"
+alias ytdla="youtube_download_audio"
